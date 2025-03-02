fb8df7debcc64c57ab054486324a1842eb81f73a
KKangHHee <skh8609@gmail.com>
2025-03-02 22:25:10 +0900

    chore(#1): recoil 설정

---
+++ src/store/Recoil.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import { RecoilRoot } from 'recoil';
+
+interface RecoilRootProps {
+  children: ReactNode;
+}
+
+const Recoil = ({ children }: RecoilRootProps) => {
+  return <RecoilRoot>{children}</RecoilRoot>;
+};
+
+export default Recoil;
