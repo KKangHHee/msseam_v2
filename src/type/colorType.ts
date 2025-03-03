e27d25220cf9156d24ae6bb30aa1e2871bbdad4a
KKangHHee <skh8609@gmail.com>
2025-03-03 20:32:35 +0900

    feat: color 정의 및 폰트 적용

---
+++ src/type/colorType.ts
@@ -0,0 +1,26 @@
+export type Color =
+  | 'black'
+  | 'white'
+  | 'gray'
+  | 'dark-gray'
+  | 'light-purple'
+  | 'purple'
+  | 'alarm-red';
+
+// export const commonColor = {
+//   black: '#222',
+//   white: '#fff',
+//   gray: '#a7a7a7',
+//   darkGray: '#83899b',
+//   lightPurple: '#dacaff',
+//   purple: '#ad71ea',
+//   alarmRed: '#ff5c5c',
+// };
+
+export const badgeColor = {
+  NEWBIE: '#80E045', // 초록색
+  엠비티라노: '#00AF76', // 청록색
+  MBTMI: '#FAA454', // 오렌지색
+  엠비티어른: '#F85CA2', // 분홍색
+  엠비티어른2: '#00B5DC', // 파란색
+};
